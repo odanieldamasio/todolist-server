867e9684c975e0f2c039168467a7d2d1d0d2bb3d
Daniel Damasio <damasio.dan7@gmail.com>
2025-01-30 17:33:55 -0300

    first commit

---
+++ src/test/java/com/danieldamasio/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danieldamasio.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
