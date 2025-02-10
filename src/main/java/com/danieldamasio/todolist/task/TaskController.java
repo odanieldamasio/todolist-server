c928990b8a99ada3044d1a36d403b55692dcf91f
Daniel Damasio <damasio.dan7@gmail.com>
2025-02-10 18:00:36 -0300

    feat: crud actions

--- src/main/java/com/danieldamasio/todolist/task/TaskController.java
+++ src/main/java/com/danieldamasio/todolist/task/TaskController.java
@@ -1,9 +1,15 @@
 package com.danieldamasio.todolist.task;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +26,29 @@ public class TaskController {
     }
     
     @GetMapping
-    public List<Task> listAll() {
+    public List<Task> listAllTasks() {
         return taskService.listAll();
+    }
+    
+    @GetMapping("/{id}")
+    public ResponseEntity<Task> findTaskById(@PathVariable Long id) {
+        Optional<Task> task = taskService.findByID(id);
+        return task.map(ResponseEntity::ok)
+            .orElseGet(() -> ResponseEntity.notFound().build()
+        );
+    }
+
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.remove(id);
+        return ResponseEntity
+            .noContent()
+            .build();
     }
 }
